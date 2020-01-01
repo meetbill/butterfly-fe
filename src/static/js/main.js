d6dc3670808e27daf2b81aeba97ddad01c9d8128
meetbill <meetbill@163.com>
2020-01-01 11:42:45 +0800

    [Changed]:update jquery 2.1.4 to 2.2.4

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -17,7 +17,7 @@ function ($scope, $translate, $localStorage, $window) {
     $scope.app = {
         //name: 'Angulr UI',
         name: 'Pine',
-        version: '2.2.3_20191230',
+        version: '2.2.4_20200101',
         // for chart colors
         color: {
             primary: '#7266ba',
